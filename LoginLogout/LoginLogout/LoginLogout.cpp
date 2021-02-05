ec4c895e76e1b3b182da4a52cd2d9cd5cd9cdc0b
Best-Code <cpmraygun@gmail.com>
2021-02-05 18:58:47 -0500

    Update fixes the withdraw / deposit not keeping track error

--- LoginLogout/LoginLogout/LoginLogout.cpp
+++ LoginLogout/LoginLogout/LoginLogout.cpp
@@ -23,6 +23,7 @@ static int callback(void* unused, int count, char** data, char** columns)
 {
     int idx;
 
+
     fromPass = data[0];
     if (toPass == fromPass)
         passCorrect = true;
@@ -34,7 +35,7 @@ static int callback(void* unused, int count, char** data, char** columns)
 static int balancc(void* unused, int count, char** data, char** columns)
 {
     int idx;
-
+    
     xbalance = atof(data[0]);
 
     printf("\n");
@@ -59,6 +60,31 @@ static int callUser(void* unused, int count, char** data, char** columns)
     return 0;
 }
 
+
+void runSQL(std::string& sql, int call)
+{
+    sqlite3* DB;
+    char* errorMsg;
+
+    int exit = sqlite3_open("uandp.db", &DB);
+
+    if (call == 1) {
+        sqlite3_exec(DB, sql.c_str(), callback, 0, &errorMsg);
+    }
+    else if (call == 2)
+    {
+        sqlite3_exec(DB, sql.c_str(), balancc, 0, &errorMsg);
+    }
+    else if (call == 3)
+    {
+        sqlite3_exec(DB, sql.c_str(), callUser, 0, &errorMsg);
+
+    }
+
+    sqlite3_close(DB);
+}
+
+
 class User
 {
 public:
@@ -72,6 +98,8 @@ public:
 
     void baseFunction()
     {
+        int a;
+        std::cin >> a;
         cls
             std::cout << "                Your last sign in was : " << lastSignIn << std::endl << std::endl;
         std::string mr = "What would you like to do today ";
@@ -96,7 +124,7 @@ Log-Out > 4
             deposit();
         }
         if (choice == 3) {
-            checkBalance();
+            checkBalance(true);
         }if (choice == 4) {
             Logout();
         }
@@ -106,14 +134,11 @@ Log-Out > 4
     void withdraw()
     {
 
-        sqlite3* DB;
-        char* errorMsg;
-
-        int exit = sqlite3_open("uandp.db", &DB);
+        
 
         cls
-            std::cout << "How much would you like to withdraw from your current balance of : " << balance << std::endl;
-            std::cout << "Type 0 to cancel!" << std::endl;
+        std::cout << "How much would you like to withdraw from your current balance of : " << balance << std::endl;
+        std::cout << "Type 0 to cancel!" << std::endl;
         bool x = false;
         int amt;
         while (!x) {
@@ -127,66 +152,66 @@ Log-Out > 4
                 std::cout << "You must not withdraw more than your current balance of : " << balance << " \nTry Again > ";
             }
         }
-        std::cout << "Your new balance is : " << balance;
-        std::string sql("UPDATE user SET balance = " + std::to_string(balance) + " WHERE username = '" + username + "'");
-        sqlite3_exec(DB, sql.c_str(), callback, 0, &errorMsg);
-        sqlite3_close(DB);
+
+        std::string sql("UPDATE user SET balance = " + std::to_string(balance) + " WHERE username = \"" + username + "\"");
+        runSQL(sql, 2);
+
+        checkBalance(false);
+        
+
         balance = xbalance;
-        std::cout << std::endl << std::endl << "Press any key to go back to the main menu.";
-        std::cin.get();
-        std::cin.get();
+
+        std::cout << std::endl << std::endl << "Type any key to go back to the main menu.";
+
         baseFunction();
     }
 
     void deposit()
     {
-
-        sqlite3* DB;
-        char* errorMsg;
-
-        int exit = sqlite3_open("uandp.db", &DB);
         
 
         cls
-            std::cout << "How much would you like to deposit from your current balance of : " << balance << std::endl;
+            std::cout << "How much would you like to deposit toward your current balance of : " << balance << std::endl;
         std::cout << "Type 0 to cancel!" << std::endl;
 
         int amt;
         std::cin >> amt;
         balance += amt;
-        std::cout << "Your new balance is : " << balance;
-        std::string sql("UPDATE user SET balance = "+std::to_string(balance)+" WHERE username = '"+username+"'");
-        sqlite3_exec(DB, sql.c_str(), callback, 0, &errorMsg);
-        sqlite3_close(DB);
+
+        std::string sql("UPDATE user SET balance = "+std::to_string(balance)+" WHERE username = \""+username+"\"");
+        runSQL(sql, 2);
+
+        checkBalance(false);
+
+
         balance = xbalance;
-        std::cout << std::endl << std::endl << "Press any key to go back to the main menu.";
-        std::cin.get();
-        std::cin.get();
+
+        std::cout << std::endl << std::endl << "Type any key to go back to the main menu.";
 
         baseFunction();
 
     }
 
-    void checkBalance()
+    void checkBalance(bool n)
     {
-
-        sqlite3* DB;
-        char* errorMsg;
-
-        int exit = sqlite3_open("uandp.db", &DB);
+        
         std::string sql("SELECT balance FROM user WHERE username = '" + username+"';");
-        sqlite3_exec(DB, sql.c_str(), balancc, 0, &errorMsg);
-        sqlite3_close(DB);
+        runSQL(sql, 2);
 
         balance = xbalance;
 
-        cls
-            std::cout << name << ", your current balance is : " << balance;
-        std::cout << std::endl << std::endl << "Press any key to go back to the main menu.";
-        std::cin.get();
-        std::cin.get();
-        baseFunction();
+        if (!n)
+            std::cout << "Your new balance is : " << balance;
 
+        
+
+        if (n) {
+            cls
+                std::cout << name << ", your current balance is : " << balance;
+            std::cout << std::endl << std::endl << "Type any key to go back to the main menu.";
+
+            baseFunction();
+        }
     }
 
     void Logout() {
@@ -227,7 +252,7 @@ public:
                 bPass = true;
             std::cout << std::endl;
         }
-        std::cout << std::endl << "Press any key to submit";
+        std::cout << std::endl << "Type any key to submit";
         std::cin.get();
         addUser(username, password, firstName, lastName);
     }
@@ -253,7 +278,6 @@ public:
         }
         User newUser;
         newUser.baseFunction();
-        
 
     }
 
@@ -261,17 +285,9 @@ public:
     int addUser(std::string& user, std::string& pass, std::string& name, std::string& lastName)
     {
 
-        sqlite3* DB;
-        char* messaggeError;
-
-        //Accessing the database
-        int exit = sqlite3_open("uandp.db", &DB);
-      //Adds a user into the database with the values they registered
-
         std::string sql("INSERT INTO user(username, password, name, lastName) VALUES('"+user+"','"+pass+"','"+name+"','"+lastName+"');");
-        exit = sqlite3_exec(DB, sql.c_str(), callback, 0, &messaggeError);
+        runSQL(sql, 1);
 
-        sqlite3_close(DB);
         return 0;
     }
 
@@ -294,26 +310,12 @@ public:
         //                                Adding a 0 if the min is less than 10 so its X:09 not X:9
         std::string lastOn = std::to_string(hour) + ":"+ ((min<10) ? "0":"") + std::to_string(min) + " on " + std::to_string(month) + "-" + std::to_string(day) + '-' + std::to_string(year);
 
-        sqlite3* DB;
-        char* messaggeError;
-        
-        //Opening up the database
-        int exit = sqlite3_open("uandp.db", &DB);
-
-        //Checks if the password entered is the users password.
-        //Then updates to the last logged on time.
         std::string sql("SELECT password FROM user WHERE username = '" + user + "';"+ "UPDATE user SET lastSignIn = '" + lastOn + "' WHERE username = '" + user + "';");
-        //Executes the sql above on the database
-        sqlite3_exec(DB, sql.c_str(), callback, 0, &messaggeError);
-
-        //Close db for no more changes
-        sqlite3_close(DB);
-
-        //Selecting everything so I can make a user class with all the variables filled 
-        exit = sqlite3_open("uandp.db", &DB);
+        runSQL(sql, 1);
+        
         sql = "SELECT * FROM user WHERE username = '" + user+"'";
-        sqlite3_exec(DB, sql.c_str(), callUser, 0, &messaggeError);
-        sqlite3_close(DB);
+
+        runSQL(sql, 3);
 
         return 0;
     }
@@ -332,8 +334,8 @@ public:
 
 Would you like to Log-in or Register?
 ------------------------
-Log-in > Press 1
-Register > Press 2
+Log-in > Type 1
+Register > Type 2
 )";
         int which;
         std::cin >> which;
